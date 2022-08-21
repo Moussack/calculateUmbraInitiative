c87102c6af08857d4d5584f757d038681c5b46c8
reyhan <reywahyono@gmail.com>
2022-08-21 21:21:20 +0700

    add tolocalsetingf

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function calcCHD(currentCHD) {
    let CHD_100_stacks = currentCHD + 100;
+   console.log(`Your Current Crit Hit Damage = ${currentCHD}%`);
    console.log(`Your Crit Hit Damage @100 stacks = ${CHD_100_stacks}%`);
    return CHD_100_stacks;
 }
@@ -7,17 +8,17 @@ function calcCHD(currentCHD) {
 function calcRps(current_rpm) {
    let RPM_100_stacks = current_rpm * 1.5;
    let roundPerSecond = RPM_100_stacks / 60;
-   console.log(`Your Current Weapon's Round Per Second @100 stacks = ${roundPerSecond} RPS`);
+   console.log(`Your Current Weapon's RPS @100 stacks = ${roundPerSecond} round per sec`);
    return roundPerSecond;
 }
 
 function showRpm(yourRpm) {
    let yourRpm100 = yourRpm * 1.5;
    let data = [];
-   console.log(`Your Current Weapon's RPM = ${yourRpm} RPM`);
-   console.log(`Your RPM @100 stacks = ${yourRpm100} RPM`);
+   console.log(`Your Current Weapon's RPM = ${yourRpm} rounds per minute`);
+   console.log(`Your RPM @100 stacks = ${yourRpm100} rounds per minute`);
    for (let i = 0; i < 25; i++) {
-      yourRpm100 = yourRpm100 - 3.6;
+      yourRpm100 = yourRpm100 - 3.6; // 3,6 is 2% rpm loss every sec
       data.push(Number(yourRpm100.toFixed(1)));
    }
 
@@ -35,24 +36,35 @@ function calcUmbra(weaponDmg, totalCHD, totalRPS) {
 let rpsData = [];
 let chdData = [];
 let dataFinal = [];
-
 let rpm = showRpm(180); // <-- ENTER YOUR CURRENT WEAPON's RPM HERE
 let rps = calcRps(180); // <-- ENTER YOUR CURRENT WEAPON's RPM HERE
 let chd = calcCHD(125); // <-- ENTER YOUR CURRENT CHD HERE
-let wpnDmg = 100; // <-- ENTER YOUR WEAPON DMG HERE
+let wpnDmg = 1200; // <-- ENTER YOUR WEAPON DMG HERE
+console.log(`Your Current Weapon's dmg = ${wpnDmg}`);
+console.log(' ');
+console.log(`------------------------DPS LOSS PER SECOND----------------------------------`);
 
 // Output
-console.log(`${calcUmbra(wpnDmg, chd, rps).toFixed(1)} DPS at ${chd}% CHD at ${rps} RPS(${270} RPM) at 0 sec`);
+console.log(
+   `${calcUmbra(wpnDmg, chd, rps).toLocaleString('en-US')} DPS(${(calcUmbra(wpnDmg, chd, rps) / rps).toLocaleString(
+      'en-US'
+   )} dmg per bullet) at ${chd}% CHD with ${rps} RPS(${270} RPM) at 0 sec`
+);
 for (let i = 0; i < 25; i++) {
-   rps = rps - 0.06;
-   rpsData.push(rps.toFixed(2));
+   rps = rps - 0.06; // 0.06 is 2% rpm loss every sec
+   rpsData.push(Number(rps.toFixed(1)));
 
-   chd = chd - 4;
+   chd = chd - 4; // 4 is 4 stackk loss every sec
    chdData.push(chd);
 
-   let dpsFinal = calcUmbra(wpnDmg, chdData[i], rpsData[i]).toFixed(1);
+   let dpsFinal = Number(calcUmbra(wpnDmg, chdData[i], rpsData[i]).toFixed(1));
    dataFinal.push(dpsFinal);
-   console.log(`${dataFinal[i]} DPS at ${chdData[i]}% CHD at ${rpsData[i]} RPS(${rpm[i]} RPM) at ${i + 1}sec`);
+
+   console.log(
+      `${dataFinal[i].toLocaleString('en-US')} DPS(${(dataFinal[i] / rpsData[i]).toLocaleString(
+         'en-US'
+      )} dmg per bullet) at ${chdData[i]}% CHD with ${rpsData[i]} RPS(${rpm[i]} RPM) at ${i + 1}sec`
+   );
 }
 
 // calc dps at certain secs
